fc68bb924099409f4cd72aff41a3e1734aae787a
Hypnoticat <spyracat@gmail.com>
2025-10-19 22:07:19 +0100

    Redundancy: - this commit includes multiple failed methods of sending input - this commit is to preserve them in the case of reverting in the future

--- LowLevelController/GeneralController.cs
+++ LowLevelController/GeneralController.cs
@@ -3,7 +3,51 @@ using System.Runtime.InteropServices;
 
 namespace LowLevelController;
 
+// struct definitions for sendInput
+[StructLayout(LayoutKind.Sequential)]
+struct Input
+{
+    public uint type;
+    public InputUnion union;
+    public static int Size => Marshal.SizeOf<Input>();
+}
 
+[StructLayout(LayoutKind.Explicit)]
+struct InputUnion
+{
+    [FieldOffset(0)] public MouseInput mi;
+    [FieldOffset(0)] public KeyboardInput ki;
+    [FieldOffset(0)] public HardwareInput hi;
+}
+
+[StructLayout(LayoutKind.Sequential)]
+struct MouseInput
+{
+    public int dx;
+    public int dy;
+    public uint mouseData;
+    public uint dwFlags;
+    public uint time;
+    public IntPtr dwExtraInfo;
+}
+
+[StructLayout(LayoutKind.Sequential)]
+struct KeyboardInput
+{
+    public ushort wVk;
+    public ushort wScan;
+    public uint dwFlags;
+    public uint time;
+    public IntPtr dwExtraInfo; 
+}
+
+[StructLayout(LayoutKind.Sequential)]
+struct HardwareInput
+{
+    public uint uMsg;
+    public ushort wParamL;
+    public ushort wParamH;
+}
 
 public class GeneralController
 {
@@ -18,6 +62,10 @@ public class GeneralController
     }
     
     private const int VK_ESCAPE = 0x1B;
+    private const int WM_KEYDOWN = 0x100;
+    private const int WM_KEYUP = 0x101;
+
+    private static int[] heldKeys = new int[255];
     
     private static IntPtr hookId = IntPtr.Zero;
     private static InputProc hkProc = HookCallback;
@@ -35,11 +83,22 @@ public class GeneralController
     [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
     private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
 
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr SendInput(uint numInputs, IntPtr buf, int bufSize);
+    
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr PostMessage(IntPtr windowHandle, uint msg, IntPtr wParam, IntPtr lParam);
+    
+    [DllImport("user32.dll")]
+    public static extern uint MapVirtualKey(uint uCode, uint uMapType);
+    
     [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
     private static extern IntPtr GetModuleHandle(string lpModuleName);
-
+    
     // the current device being monitored
     private static DeviceType inputDevice = DeviceType.Unset;
+    
+    private static Process? targetProc = null;
     
     /// <summary>
     /// Inserts the given hook process into the hook chain for the given process
@@ -47,13 +106,13 @@ public class GeneralController
     /// <param name="hkProc">The hook code</param>
     /// <param name="targetProc">The process to insert the hook into</param>
     /// <returns>0 for success, 1 for process non-existent</returns>
-    private static IntPtr SetHook(InputProc hkProc, Process targetProc)
+    private static IntPtr SetHook(InputProc hkProc)
     {
         // insert the hook into the chain
-        if (targetProc.MainModule != null)
+        Process curProc = Process.GetCurrentProcess();
+        if (curProc.MainModule != null)
         {
-            ProcessModule module = targetProc.MainModule;
-            return SetWindowsHookEx((int)inputDevice, hkProc, GetModuleHandle(module.ModuleName), 0);
+            return SetWindowsHookEx((int)inputDevice, hkProc, GetModuleHandle(curProc.MainModule.ModuleName), 0);
         }
         return 1;
     }
@@ -68,13 +127,97 @@ public class GeneralController
     private static IntPtr HookCallback(int code, IntPtr wParam, IntPtr lParam)
     {
         // read input and decide what to do with it
-        if (code >= 0 && inputDevice == DeviceType.Keyboard && wParam == VK_ESCAPE)
+        int vkCode = Marshal.ReadInt32(lParam);
+        if (code >= 0 && inputDevice == DeviceType.Keyboard && wParam == WM_KEYDOWN && vkCode == VK_ESCAPE)
         {
+            Console.WriteLine("escaped the hook");
             if(hookId != 0){ return RemoveHook(); }
         }
+        else
+        {
+            if (targetProc != null)
+            {
+                /*
+                uint scanCode = MapVirtualKey((uint)vkCode, 0);
+                uint newParams = 1
+                                 | (scanCode << 16)
+                                 | (0)
+                                 | (0)
+                                 | (uint)(wParam == WM_KEYUP ? 1 << 30 : 0)
+                                 | (uint)(wParam == WM_KEYUP ? 1 << 31: 0);
+                PostMessage(targetProc.Handle, (uint)wParam, (IntPtr)vkCode, (IntPtr)newParams);
+                if ((heldKeys[vkCode] == 0 && wParam == WM_KEYDOWN) || (heldKeys[vkCode] == 1 && wParam == WM_KEYUP))
+                {
+                    
+                }
+                
+                if(vkCode > 255 || vkCode < 0){ return 1; }
+                else if (wParam == WM_KEYDOWN)
+                {
+                    heldKeys[vkCode] = 1;
+                }
+                else if (wParam == WM_KEYUP)
+                {
+                    heldKeys[vkCode] = 0;
+                }*/
+                
+                /*if ((heldKeys[vkCode] == 0 && wParam == WM_KEYDOWN) || (heldKeys[vkCode] == 1 && wParam == WM_KEYUP))
+                {
+                    uint dwDevice;
+                    switch(inputDevice)
+                    {
+                        case DeviceType.Mouse:
+                            dwDevice = 0;
+                            break;
+                        case DeviceType.Keyboard:
+                            dwDevice = 1;
+                            break;
+                        default:
+                            dwDevice = 2;
+                            break;
+                    }
+
+                    uint dwAct;
+                    switch (wParam)
+                    {
+                        case WM_KEYDOWN:
+                            dwAct = 0;
+                            break;
+                        case WM_KEYUP:
+                            dwAct = 2;
+                            break;
+                        default:
+                            dwAct = 0;
+                            break;
+                    }
+
+                    Input inp = new Input
+                    {
+                        type = dwDevice,
+                        union = new InputUnion
+                        {
+                            ki = new KeyboardInput
+                            {
+                                wVk = (ushort)vkCode,
+                                wScan = 0,
+                                dwFlags = dwAct,
+                                time = 0,
+                                dwExtraInfo = 0
+                            }
+                        }
+                    };
+                
+                    IntPtr inpLoc = Marshal.AllocHGlobal(Marshal.SizeOf<Input>());
+                    Marshal.StructureToPtr(inp, inpLoc, false);
+                    SendInput(1, inpLoc, Input.Size);
+                }*/
+            }
+        }
+        Console.WriteLine($"hook was called");
         
         // continue the input hook chain
-        return CallNextHookEx(hookId, code, wParam, lParam);
+        //return CallNextHookEx(hookId, code, wParam, lParam);
+        return 1;
     }
 
     /// <summary>
@@ -90,22 +233,28 @@ public class GeneralController
     /// Sets the process to attach the hook to
     /// </summary>
     /// <param name="process">The process to attach to(has type Process, DO NOT PASS THE NAME)</param>
-    public static void SetProcess(Process process)
+    public void SetProcess(Process? process)
     {
-        if (inputDevice != DeviceType.Unset)
-        {
-            if(hookId != 0){ RemoveHook(); }
-            hookId = SetHook(hkProc, process); 
-        }
+        if(hookId != 0){ RemoveHook(); }
+        targetProc = process;
     }
 
     /// <summary>
     /// Sets the input device to interrupt
     /// </summary>
     /// <param name="device">The type of device</param>
-    public static void SetDevice(DeviceType device)
+    public void SetDevice(DeviceType device)
     {
         if(hookId != 0){ RemoveHook(); }
         inputDevice = device;
+    }
+
+    public void AddHook()
+    {
+        if (inputDevice != DeviceType.Unset && targetProc != null)
+        {
+            if(hookId != 0){ RemoveHook(); }
+            hookId = SetHook(hkProc); 
+        }
     }
 }
--- LowLevelController/MainWindow.xaml.cs
+++ LowLevelController/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Management;
 
 namespace LowLevelController;
 
@@ -16,10 +18,53 @@ namespace LowLevelController;
 /// </summary>
 public partial class MainWindow : Window
 {
+    List<Process> runningProcs = new List<Process>();
+    private GeneralController keyboardController;
+    
     public MainWindow()
     {
         InitializeComponent();
+        
+        keyboardController = new GeneralController();
+        keyboardController.SetDevice(GeneralController.DeviceType.Keyboard);
+        MonitorProcesses();
+    }
 
-        GeneralController keyboardController = new GeneralController();
+    public void MonitorProcesses()
+    {
+        foreach (Process proc in Process.GetProcesses().Where(proc => proc.MainWindowHandle != 0))
+        {
+            ProcessChoice.Items.Add(proc.ProcessName);
+        }
+        ManagementEventWatcher procRun = new ManagementEventWatcher("SELECT * FROM Win32_ProcessStartTrace");
+        ManagementEventWatcher procTerm = new ManagementEventWatcher("SELECT * FROM Win32_ProcessStopTrace");
+
+
+        procRun.EventArrived += (s, e) =>
+        {
+            Process proc = (Process) e.NewEvent.Properties["Process"].Value;
+            if (proc != null && proc.MainWindowHandle != 0)
+            {
+                ProcessChoice.Items.Add(proc.ProcessName);
+            }
+        };
+
+        procTerm.EventArrived += (s, e) =>
+        {
+            Process proc = (Process) e.NewEvent.Properties["Process"].Value;
+            if (proc != null && runningProcs.Contains(proc) && proc.MainWindowHandle != 0)
+            {
+                ProcessChoice.Items.Remove(proc.ProcessName);
+            }
+        };
+
+        procRun.Start();
+        procTerm.Start();
+    }
+
+    public void AddHook(object sender, RoutedEventArgs e)
+    {
+        keyboardController.SetProcess(Process.GetProcessesByName((string)ProcessChoice.SelectedValue)[0]);
+        keyboardController.AddHook();
     }
 }
